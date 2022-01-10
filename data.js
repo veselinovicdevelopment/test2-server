2e619049bd3a67ac1ad0d5d7f2926a10fcebc5f4
niceDev0908 <veselinovicdevelopment@gmail.com>
2022-01-10 05:27:25 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,102 @@
+const crumbs = ["Home", "Tools", "Suche"];
+
+const cats = ["Programmierung", "Informatik", "Robotik", "Buchführung", "Ingenieurwissenschaft & Technik", "Mathematik"];
+
+const books = [
+  {
+    autor: "Ulrich Bröckl",
+    titel: "C als erste Programmiersprache: Vom Einsteiger zum Fortgeschrittenen",
+    isbn: "3834812218",
+    preis: 27.99,
+    lieferbar: true,
+    kat: "Programmierung"
+  },
+  {
+    autor: "Uwe Haneke",
+    titel: "Open Source Business Intelligence (OSBI): Möglichkeiten, Chancen und Risiken quelloffener BI-Lösungen",
+    isbn: "3446423966",
+    preis: 17.00,
+    lieferbar: true,
+    kat: "Informatik"
+  },
+  {
+    autor: "Uwe Haneke",
+    titel: "Data Science: Grundlagen, Architekturen und Anwendungen",
+    isbn: "3864908221",
+    preis: 59.90,
+    lieferbar: true,
+    kat: "Informatik"
+  },
+  {
+    autor: "Uwe Haneke",
+    titel: "Das LEGO-Mindstorms-Handbuch: Spielend zur Informatik mit EV3-Robotern",
+    isbn: "3864904242",
+    preis: 27.90,
+    lieferbar: true,
+    kat: "Robotik"
+  },
+  {
+    autor: "Michael Reichhardt",
+    titel: "Grundlagen der doppelten Buchführung: Schritt Für Schritt Einfach Erklärt Fast 70 Übungen Mit Lösungen",
+    isbn: "3658145021",
+    preis: 32.99,
+    lieferbar: true,
+    kat: "Buchführung"
+  },
+  {
+    autor: "Peter A. Henning",
+    titel: "Smart Home Hacks: Hausautomatisierung selber machen",
+    isbn: "3960090129",
+    preis: 19.70,
+    lieferbar: true,
+    kat: "Ingenieurwissenschaft & Technik"
+  },
+  {
+    autor: "Dirk W. Hoffmann",
+    titel: "Grenzen der Mathematik: Eine Reise durch die Kerngebiete der mathematischen Logik",
+    isbn: "978-3662566169",
+    preis: 29.99,
+    lieferbar: true,
+    kat: "Programmierung"
+  },
+  {
+    autor: "Dirk W. Hoffmann",
+    titel: "Einsteins Relativitätstheorie: Eine geführte Reise durch Raum, Zeit und die Geschichte der Physik",
+    isbn: "978-3738615784",
+    preis: 19.95,
+    lieferbar: false,
+    kat: "Ingenieurwissenschaft & Technik"
+  },
+  {
+    autor: "Dirk W. Hoffmann",
+    titel: "Forcing: Eine Einführung in die Mathematik der Unabhängigkeitsbeweise",
+    isbn: "978-3746044606",
+    preis: 34.90,
+    lieferbar: true,
+    kat: "Mathematik"
+  },
+  {
+    autor: "Matthias Wölfel ",
+    titel: "Distant Speech Recognition",
+    isbn: "978-0470517048",
+    preis: 109.87,
+    lieferbar: true,
+    kat: "Informatik"
+  },
+  {
+    autor: "Udo Müller",
+    titel: "Perl - Grundlagen, fortgeschrittene Techniken und Übungen",
+    isbn: "978-3826617768",
+    preis: 7.90,
+    lieferbar: true,
+    kat: "Programmierung"
+  },
+  {
+    autor: "Udo Müller",
+    titel: "Java - Das Lehrbuch",
+    isbn: "978-3826613333",
+    preis: 19.90,
+    lieferbar: false,
+    kat: "Programmierung"
+  }
+];
---
+++ main.js
@@ -0,0 +1,65 @@
+$(document).ready(function (e) {
+	$(".input-group .dropdown-item").click(function (e) {
+		e.preventDefault();
+		var value = $(this).text();
+		$('#category').text(value);
+	});
+
+	$('.form').submit(function (e) {
+		e.preventDefault();
+		search();
+	});
+
+	search();
+
+	function search() {
+		var category = $("#category").text();
+		if (category == 'Kategorien') {
+			category = null;
+		}
+
+		var available = $('#available').prop('checked');
+		var searchText = $("#search-text").val();
+
+		var result = books;
+		if (available) {
+			result = result.reduce((acc, cur) => {
+				if (cur.lieferbar) {
+					return [...acc, cur];
+				}
+				return acc;
+			}, []);
+		}
+
+		if (category) {
+			result = result.reduce((acc, cur) => {
+				if (cur.kat == category) {
+					return [...acc, cur];
+				}
+				return acc;
+			}, []);
+		}
+
+		if (searchText) {
+			result = result.reduce((acc, cur) => {
+				if (cur.titel.includes(searchText) || cur.autor.includes(searchText)) {
+					return [...acc, cur];
+				}
+
+				return acc;
+			}, []);
+		}
+
+		var html = '';
+		result.forEach((user, index) => {
+			html += '<div class="user-box">\
+						<p><span>Autor: </span>' + user.autor + '</p>\
+						<p><span>Titel: </span>' + user.titel + '</p>\
+						<p><span>Isbn: </span>' + user.isbn + '</p>\
+						<p><span>Price: </span>' + user.preis + '</p>\
+					</div>';
+		});
+
+		$(".search-result-box").html(html);
+	}
+});
